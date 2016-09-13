ab3d2567523edf27b2014b3daa8b19c5af87807e
TomaszWu <tomasz.wojciechowski84@gmail.com>
2016-09-13 20:14:36 +0200

    removeUseCase

--- useCase/tweetPractice.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-        require_once '../src/User.php';
-        require_once '../src/Tweet.php';
-        require_once 'connection.php';
-        
-        $tweet = Tweet::loadTweetById($conn, 5);
-
-var_dump($tweet);
